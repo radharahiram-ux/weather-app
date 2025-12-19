4cd3240082a1c901806d2ec940ba654c61a7f513
kavyansh krishan <radharahiram@gmail.com>
2025-12-19 12:18:09 +0530

    Add files via upload

---
+++ weather_app-main/README.md
@@ -0,0 +1,14 @@
+# weatherapp
+Created by Kavya @kavyansh .
+
+Utilises CSS ,HTML,Js.
+
+Utilises concept of API to search a database.
+
+Simple yet efficient weather searching app!
+
+To run locally: Start a local server (e.g., python -m http.server 8000) and visit http://localhost:8000
+Anyone interested in upgrading this website is surely welcome!
+
+Wait! There is more to come!
+                              
---
+++ weather_app-main/script2.js
@@ -0,0 +1,40 @@
+const url =
+	'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+	'70195b4eb13086cdad789b3155545592';
+
+$(document).ready(function () {
+	weatherFn('Ponda');
+});
+
+async function weatherFn(cName) {
+	const temp =
+		`${url}?q=${cName}&appid=${apiKey}&units=metric`;
+	try {
+		const res = await fetch(temp);
+		const data = await res.json();
+		if (res.ok) {
+			weatherShowFn(data);
+		} else {
+			alert('City not found. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+function weatherShowFn(data) {
+	$('#city-name').text(data.name);
+	$('#date').text(moment().
+		format('MMMM Do YYYY, h:mm a'));
+	$('#temperature').
+		html(`${data.main.temp}°C`);
+	$('#description').
+		text(data.weather[0].description);
+	$('#wind-speed').
+		html(`Wind Speed: ${data.wind.speed} m/s`);
+	$('#weather-icon').
+		attr('src',
+			`...`);
+	$('#weather-info').fadeIn();
+}
